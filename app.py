81290adbbd2b8be1776b1abef88ed4c01e6740df
xixuy <15764241610@163.com>
2019-07-09 15:19:18 +0800

    flask基本语法

--- app.py
+++ app.py
@@ -1,3 +1,5 @@
+import json
+
 from flask import Flask, request
 
 #初始化
@@ -15,12 +17,12 @@ class Config(object):
 
 app.config.from_object(Config)
 
-#======从文件中加载配置======
-
-app.config.from_pyfile('config.ini')
-
-#======从环境变量中加载配置====
-app.config.from_envvar()
+# #======从文件中加载配置======
+#
+# app.config.from_pyfile('config.ini')
+#
+# #======从环境变量中加载配置====
+# app.config.from_envvar()
 
 
 #使用装饰器路由去与试图函数进行关联
@@ -33,10 +35,27 @@ def hello_world():
 def demo1():
     return 'ni hao'
 
-
-@app.route('/dem2',methods=['GET','POST'])
+#指定请求方式
+@app.route('/demo2',methods=['GET','POST'])
 def demo2():
     return 'demo2 %s' % request.methods
+
+
+#给路由添加参数
+@app.route('/demo3/<int:user_id>')
+def demo3(user_id):
+    return 'demo3 %s' % user_id
+
+
+@app.route('/demo4')
+def demo4():
+    json_dict={
+        "name":"xiaoxi",
+        "age":18
+    }
+    #使用json.dumps将字典转换成JSON字符串，使用json.loads将JSON字符串转换为字典
+    result=json.dumps(json_dict)
+    return result
 
 
 if __name__ == '__main__':
